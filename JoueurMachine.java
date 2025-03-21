61f215e654851a00902d181d4f89165d2dceb8aa
alhDiallo2018 <160555669+alhDiallo2018@users.noreply.github.com>
2025-03-21 01:07:51 +0000

    Delete JoueurMachine.java

--- JoueurMachine.java
+++
@@ -1,8 +0,0 @@
-public class JoueurMachine extends Joueur {
-    public int niveau;
-
-    public JoueurMachine(String nom, int niveau, int index) {
-        super(nom, index);
-        this.niveau = niveau;
-    }
-}
